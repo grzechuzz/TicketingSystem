5cdfe2cb8badc49fdb3d142cf4f3b2fd1e0c8171
Grzegorz Rzepiela <grzegorz.rzepiela@student.pk.edu.pl>
2025-07-14 22:22:19 +0200

    feat: add get_user_by_email function

--- app/domain/users/crud.py
+++ app/domain/users/crud.py
@@ -1,8 +1,13 @@
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy import select, exists
+from sqlalchemy import select
 from .models import Role, User
 
 async def get_role_by_name(name: str, db: AsyncSession) -> Role | None:
     stmt = select(Role).where(Role.name == name)
+    result = await db.execute(stmt)
+    return result.one_or_none()
+
+async def get_user_by_email(email: str, db: AsyncSession) -> User | None:
+    stmt = select(User).where(User.email == email)
     result = await db.execute(stmt)
     return result.one_or_none()
