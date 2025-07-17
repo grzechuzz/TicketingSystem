8b48904f4a0213ed0ebae33d3351852b13b75d93
Grzegorz Rzepiela <grzegorz.rzepiela@student.pk.edu.pl>
2025-07-17 17:25:59 +0200

    chore: add Venue to init.py

--- app/domain/__init__.py
+++ app/domain/__init__.py
@@ -2,7 +2,8 @@ from .associations import user_roles, organizers_users
 from .addresses.models import Address
 from .organizers.models import Organizer
 from .users.models import User, Role
+from .venues.models import Venue
 
 __all__ = (
-    "user_roles", "organizers_users", "Address", "Organizer", "User", "Role"
+    "user_roles", "organizers_users", "Address", "Organizer", "User", "Role", "Venue"
 )
