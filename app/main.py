242670a588f2efab781f4a27663c619fcf9aa40b
Grzegorz Rzepiela <grzegorz.rzepiela@student.pk.edu.pl>
2025-07-17 15:18:19 +0200

    feat: add organizers router to main.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
-from app.api.v1.routes import auth, addresses
+from app.api.v1.routes import auth, addresses, organizers
 
 app = FastAPI()
 app.include_router(auth.router)
 app.include_router(addresses.router)
+app.include_router(organizers.router)
