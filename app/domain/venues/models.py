267cf268d7fa462225204a3228db64fa9f8ef203
Grzegorz Rzepiela <grzegorz.rzepiela@student.pk.edu.pl>
2025-07-19 17:46:03 +0200

    refactor: address_id is now obligatory

--- app/domain/venues/models.py
+++ app/domain/venues/models.py
@@ -8,9 +8,6 @@ class Venue(Base):
 
     id: Mapped[int] = mapped_column(Identity(always=True), primary_key=True)
     name: Mapped[str] = mapped_column(Text, nullable=False)
-    address_id: Mapped[int | None] = mapped_column(
-                                        ForeignKey("addresses.id", ondelete='SET NULL'),
-                                        nullable=True,
-                                        unique=True)
+    address_id: Mapped[int] = mapped_column(ForeignKey("addresses.id", ondelete='RESTRICT'), unique=True)
 
     address: Mapped['Address'] = relationship(back_populates="venues", lazy='selectin')
