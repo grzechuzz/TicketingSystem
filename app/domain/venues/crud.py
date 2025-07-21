e784014579d5d4abb2213b8bbf5c895b89642e5d
Grzegorz Rzepiela <grzegorzrzepiela5@gmail.com>
2025-07-22 00:44:22 +0200

    refactor: formatting fix

--- app/domain/venues/crud.py
+++ app/domain/venues/crud.py
@@ -32,7 +32,7 @@ async def list_all_sectors(db: AsyncSession) -> list[Sector]:
     result = await db.execute(stmt)
     return result.scalars().all()
 
-async def create_sector(db: AsyncSession, data: dict) -> Sector
+async def create_sector(db: AsyncSession, data: dict) -> Sector:
     sector = Sector(**data)
     db.add(sector)
     return sector
