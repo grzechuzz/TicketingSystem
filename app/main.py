dd9b45c1b9f2298012d8ad3685c70971ae28c5a6
Grzegorz Rzepiela <grzegorzrzepiela5@gmail.com>
2025-07-29 17:55:27 +0200

    feat: add missing routers

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
-from app.api.v1.routes import auth, addresses, organizers, venues
+from app.api.v1.routes import auth, addresses, organizers, venues, sectors, events
 
 app = FastAPI()
 app.include_router(auth.router)
 app.include_router(addresses.router)
 app.include_router(organizers.router)
 app.include_router(venues.router)
+app.include_router(sectors.router)
+app.include_router(events.router)
