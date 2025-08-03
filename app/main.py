0dbfd322652bfc49717f47241889e0009f9aa0cc
Grzegorz Rzepiela <grzegorz.rzepiela@student.pk.edu.pl>
2025-08-04 01:27:56 +0200

    feat: add seats router to main file

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from app.api.v1.routes import auth, addresses, organizers, venues, sectors, events
+from app.api.v1.routes import auth, addresses, organizers, venues, sectors, events, seats
 
 app = FastAPI()
 app.include_router(auth.router)
@@ -8,3 +8,4 @@ app.include_router(organizers.router)
 app.include_router(venues.router)
 app.include_router(sectors.router)
 app.include_router(events.router)
+app.include_router(seats.router)
