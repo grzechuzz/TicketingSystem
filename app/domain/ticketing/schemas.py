b91930a41acdc592bbf32f4d232d395c137f99da
Grzegorz Rzepiela <grzegorz.rzepiela@student.pk.edu.pl>
2025-08-07 14:23:57 +0200

    feat: add schemas for TicketType model

--- app/domain/ticketing/schemas.py
+++ app/domain/ticketing/schemas.py
@@ -20,3 +20,16 @@ class EventSectorReadDTO(BaseModel):
     event_id: int
     sector_id: int
     tickets_left: int | None
+
+
+class TicketTypeCreateDTO(BaseModel):
+    model_config = ConfigDict(extra='forbid')
+
+    name: str = Field(min_length=2, max_length=100)
+
+
+class TicketTypeReadDTO(BaseModel):
+    model_config = ConfigDict(from_attributes=True, extra='forbid')
+
+    id: int
+    name: str
