5a7d3900da1621f2cd32015f2341895d810fcbe2
Grzegorz Rzepiela <grzegorz.rzepiela@student.pk.edu.pl>
2025-08-08 14:59:40 +0200

    chore: edit init file after new models addition

--- app/domain/__init__.py
+++ app/domain/__init__.py
@@ -5,8 +5,10 @@ from .users.models import User, Role
 from .venues.models import Venue, Sector, Seat
 from .events.models import Event
 from .event_catalog.models import EventSector, TicketType, EventTicketType
+from .booking.models import TicketHolder, TicketInstance, PaymentMethod, Payment, Order
 
 __all__ = (
     "user_roles", "organizers_users", "Address", "Organizer", "User", "Role", "Venue", "Seat", "Sector",
-    "Event", "EventSector", "TicketType", "EventTicketType"
+    "Event", "EventSector", "TicketType", "EventTicketType", "TicketHolder", "TicketInstance", "PaymentMethod",
+    "Payment", "Order"
 )
