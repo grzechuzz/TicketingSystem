ac9c478595c3f9ba44043c1e204b44ab2b75234e
Grzegorz Rzepiela <grzegorz.rzepiela@student.pk.edu.pl>
2025-08-22 18:38:53 +0200

    feat: add new routers to main.py (payments, payment_methods, cart, booking)

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from app.api.v1.routes import (auth, addresses, organizers, venues, sectors, events, seats, ticket_types,
-                               event_ticket_types, booking)
+                               event_ticket_types, booking, cart, payment_methods, payments)
 
 app = FastAPI()
 app.include_router(auth.router)
@@ -13,3 +13,6 @@ app.include_router(seats.router)
 app.include_router(ticket_types.router)
 app.include_router(event_ticket_types.router)
 app.include_router(booking.router)
+app.include_router(cart.router)
+app.include_router(payment_methods.router)
+app.include_router(payments.router)
