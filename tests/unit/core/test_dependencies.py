dcffbe00c7d5ea5213a9ded3efa8f137e373f139
Grzegorz Rzepiela <grzegorz.rzepiela@student.pk.edu.pl>
2025-09-05 23:24:51 +0200

    test: refactor test_dependencies to use helper functions

--- tests/unit/core/test_dependencies.py
+++ tests/unit/core/test_dependencies.py
@@ -3,47 +3,7 @@ from fastapi import HTTPException, status
 from jose import JWTError
 from app.core import dependencies
 from sqlalchemy import Select
-
-
-def create_db(mocker, value):
-    fake_scalars = mocker.Mock()
-    fake_scalars.first.return_value = value
-    fake_result = mocker.Mock()
-    fake_result.scalars.return_value = fake_scalars
-    db = mocker.Mock()
-    db.execute = mocker.AsyncMock(return_value=fake_result)
-    return db, fake_result, fake_scalars
-
-
-def create_role(mocker, name: str):
-    role = mocker.Mock()
-    role.name = name
-    return role
-
-
-def create_organizer(mocker, organizer_id: int):
-    organizer = mocker.Mock()
-    organizer.id = organizer_id
-    return organizer
-
-
-def create_event(mocker, organizer_id: int):
-    event = mocker.Mock()
-    event.organizer_id = organizer_id
-    return event
-
-
-def create_user(mocker, roles=None, organizers=None):
-    user = mocker.Mock()
-    user.roles = roles
-    user.organizers = organizers
-    return user
-
-
-def create_event_ticket_type(mocker, event_sector_event_id: int):
-    event_ticket_type = mocker.Mock()
-    event_ticket_type.event_sector.event_id = event_sector_event_id
-    return event_ticket_type
+from tests.helper import create_role, create_user, create_db, create_organizer, create_event, create_event_ticket_type
 
 
 @pytest.mark.asyncio
