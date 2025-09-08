d7410536fac05e01c9bb7d4d7024448f0dc2756b
Grzegorz Rzepiela <grzegorz.rzepiela@student.pk.edu.pl>
2025-09-08 19:14:38 +0200

    refactor: remove not used functions

--- tests/helper.py
+++ tests/helper.py
@@ -12,35 +12,3 @@ def create_role(mocker, name: str):
     role = mocker.Mock()
     role.name = name
     return role
-
-
-def create_organizer(mocker, organizer_id: int):
-    organizer = mocker.Mock()
-    organizer.id = organizer_id
-    return organizer
-
-
-def create_event(mocker, organizer_id: int):
-    event = mocker.Mock()
-    event.organizer_id = organizer_id
-    return event
-
-
-def create_user(mocker, roles=None, organizers=None):
-    user = mocker.Mock()
-    user.roles = roles
-    user.organizers = organizers
-    return user
-
-
-def create_event_ticket_type(mocker, event_sector_event_id: int):
-    event_ticket_type = mocker.Mock()
-    event_ticket_type.event_sector.event_id = event_sector_event_id
-    return event_ticket_type
-
-
-def create_address(mocker, venue=None, organizers=None):
-    address = mocker.Mock()
-    address.venue = venue
-    address.organizers = organizers
-    return address
