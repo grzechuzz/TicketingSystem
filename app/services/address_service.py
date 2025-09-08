400aee9c929657226ecc008db33364837a1f8629
Grzegorz Rzepiela <grzegorz.rzepiela@student.pk.edu.pl>
2025-09-08 20:11:30 +0200

    refactor: edit list addresses to add pagination

--- app/services/address_service.py
+++ app/services/address_service.py
@@ -1,8 +1,9 @@
 from fastapi import HTTPException, status
 from sqlalchemy.ext.asyncio import AsyncSession
+from app.core.pagination import PageDTO
 from app.domain.addresses import crud
 from app.domain.addresses.models import Address
-from app.domain.addresses.schemas import AddressCreateDTO, AddressPutDTO
+from app.domain.addresses.schemas import AddressCreateDTO, AddressPutDTO, AddressesQueryDTO, AddressReadDTO
 from app.domain.users.models import User
 
 
@@ -13,8 +14,17 @@ async def get_address(db: AsyncSession, address_id: int) -> Address:
     return address
 
 
-async def list_addresses(db: AsyncSession) -> list[Address]:
-    return await crud.list_all_addresses(db)
+async def list_addresses(db: AsyncSession, query: AddressesQueryDTO) -> PageDTO[AddressReadDTO]:
+    addresses, total = await crud.list_all_addresses(db, query.page, query.page_size)
+
+    items = [AddressReadDTO.model_validate(address) for address in addresses]
+
+    return PageDTO[AddressReadDTO](
+        items=items,
+        total=total,
+        page=query.page,
+        page_size=query.page_size
+    )
 
 
 async def create_address(db: AsyncSession, schema: AddressCreateDTO) -> Address:
