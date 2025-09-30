f497184aa9b2b34f6dc2839fbb0beec9a47d0195
Grzegorz Rzepiela <grzegorz.rzepiela@student.pk.edu.pl>
2025-09-30 17:17:17 +0200

    refactor(test): edit helper

--- tests/helper.py
+++ tests/helper.py
@@ -1,8 +1,8 @@
 def create_db(mocker, value):
     fake_scalars = mocker.Mock()
-    fake_scalars.first.return_value = value
+    fake_scalars.first = mocker.Mock(return_value=value)
     fake_result = mocker.Mock()
-    fake_result.scalars.return_value = fake_scalars
+    fake_result.scalars = mocker.Mock(return_value=fake_scalars)
     db = mocker.Mock()
     db.execute = mocker.AsyncMock(return_value=fake_result)
     return db, fake_result, fake_scalars
