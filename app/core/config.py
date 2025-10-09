e774cb10200a0a7819c17156014331d1b8b2ce9d
Grzegorz Rzepiela <grzegorz.rzepiela@student.pk.edu.pl>
2025-10-09 15:38:19 +0200

    refactor: get postgres_user from .env, remove it from secrets

--- app/core/config.py
+++ app/core/config.py
@@ -9,16 +9,18 @@ def get_secret(secret_name: str) -> str | None:
         return os.getenv(secret_name)
 
 
-DB_USER = get_secret('db_user')
 DB_PASSWORD = get_secret('db_password')
 SECRET_KEY = get_secret('secret_key')
 REFRESH_TOKEN_PEPPER = get_secret('refresh_token_pepper')
 
 POSTGRES_DB = os.getenv("POSTGRES_DB")
+POSTGRES_USER = os.getenv("POSTGRES_USER")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
 REDIS_URL = os.getenv("REDIS_URL")
 
-if DB_USER and DB_PASSWORD and POSTGRES_DB:
-    DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@postgres:5432/{POSTGRES_DB}"
+if POSTGRES_USER and DB_PASSWORD and POSTGRES_DB:
+    DATABASE_URL = f"postgresql+asyncpg://{POSTGRES_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{POSTGRES_DB}"
 else:
     raise ValueError("Can't build DATABASE_URL")  # TODO - implement custom exception class
 
